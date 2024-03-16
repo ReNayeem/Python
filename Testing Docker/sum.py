549271b7796c16b79046809eada801eb492fc79e
Md. Nayeem <renayeem@gmail.com>
2024-03-16 11:32:42 +0600

    Docker implemented py code
    - docker run
    - docker compose

---
+++ Testing Docker/sum.py
@@ -0,0 +1,8 @@
+print('Program to sum two user input numbers')
+
+num1 = int(input('Enter the first number: '))
+num2 = int(input('Enter the second number: '))
+
+result = num1 + num2
+
+print(f'The sum of the two numbers are {result}')
